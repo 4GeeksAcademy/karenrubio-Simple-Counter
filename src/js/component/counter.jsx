55291455273436b0c6d71de98b084582d4745c0c
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-03-22 18:00:42 +0000

    no funciona xd

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+
+//crear un componente que se repita 6 veces
+//crear un componente que sea un poco mas ancho
+//crear funcionalidad contador del 1-9
+//que el contador se reinicie cuando recargue pagina
+//que al llegar a 9 el componente de la izquieeda inicie en 1
+
+
+const Counter = (props) => {
+    
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+              <i class="bi bi-clock"></i>
+            </div>
+            <div className="four card text-bg-dark mb-3" style="max-width: 18rem;">{props.digitFour}</div>
+            <div className="three card text-bg-dark mb-3" style="max-width: 18rem;">{props.digitThree}</div>
+            <div className="two card text-bg-dark mb-3" style="max-width: 18rem;">{props.digitTwo}</div>
+            <div className="one card text-bg-dark mb-3" style="max-width: 18rem;">{props.digitOne}</div>
+        </div>
+    );
+};
+
+let counterSeconds = 0;
+setInterval(function(){
+    const four = counterSeconds.toFixed(4)/1000
+    const three = counterSeconds.toFixed(3)/100
+    const two = counterSeconds.toFixed(2)/10
+    const one = counterSeconds.toFixed(1)
+    counter++
+
+},1000
+)
+
+           
+            
+ 
+export default Counter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,18 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Counter from "./counter";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		  <Counter />
+		  <Counter digitFour={four} />
+		  <Counter digitThree={three}/>
+		  <Counter digitTwo={two}/>
+		  <Counter digitOne={one} />
+		</>
 	);
 };
 
